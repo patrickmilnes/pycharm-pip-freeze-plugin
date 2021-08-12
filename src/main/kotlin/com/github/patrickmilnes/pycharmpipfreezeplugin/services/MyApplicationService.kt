293659e1f8852b4078d43d656a8d26d98366c752
GitHub Action <action@github.com>
2021-08-12 13:05:07 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/patrickmilnes/pycharmpipfreezeplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.patrickmilnes.pycharmpipfreezeplugin.services
+
+import com.github.patrickmilnes.pycharmpipfreezeplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/patrickmilnes/pycharmpipfreezeplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/patrickmilnes/pycharmpipfreezeplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.patrickmilnes.pycharmpipfreezeplugin.services
 
+import com.github.patrickmilnes.pycharmpipfreezeplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
